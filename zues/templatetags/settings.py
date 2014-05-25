48af43c0d689f94f55b2b81ced5669e97eaf2981
Tom van Dijk <t.vandijk@gmail.com>
2014-05-25 11:37:21 +0200

    Toegevoegd 'naamkort' in settings.py komt terug in <title> tag

--- zues/templatetags/settings.py
+++ zues/templatetags/settings.py
@@ -3,7 +3,13 @@ from django.conf import settings
 from django.utils.safestring import mark_safe
 
 register = template.Library()
+
 @register.simple_tag
 def mededeling():
     if hasattr(settings, 'MEDEDELING'): return mark_safe(settings.MEDEDELING)
+    else: return ""
+
+@register.simple_tag
+def naamkort():
+    if hasattr(settings, 'NAAMKORT'): return mark_safe(settings.NAAMKORT)
     else: return ""
