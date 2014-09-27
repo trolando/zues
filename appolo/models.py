3bcd36a063b112edb657a739287c6a2db3141746
root <root@mail.jongedemocraten.nl>
2014-09-27 19:57:59 +0200

    Correct meervoud modellen van appolo

--- appolo/models.py
+++ appolo/models.py
@@ -7,10 +7,16 @@ class Locatie(models.Model):
     lat = models.FloatField()
     long = models.FloatField()
 
+    class Meta:
+        verbose_name_plural = 'locaties'
+
 class Dag(models.Model):
     def __unicode__(self):
         return unicode(self.datum)
     datum = models.DateField()
+
+    class Meta:
+        verbose_name_plural = 'dagen'
 
 class Activiteit(models.Model):
     def __unicode__(self):
@@ -21,13 +27,22 @@ class Activiteit(models.Model):
     dag = models.ForeignKey(Dag)
     locatie = models.ForeignKey(Locatie)
 
+    class Meta:
+        verbose_name_plural = 'activiteiten'
+
 class Nieuwsitem(models.Model):
     def __unicode__(self):
         return self.titel
     titel = models.CharField(max_length=200)
     tekst = models.TextField()
 
+    class Meta:
+        verbose_name_plural = 'nieuwsitems'
+
 class Hashtag(models.Model):
     def __unicode__(self):
         return self.tekst
     tekst = models.CharField(max_length=200)
+
+    class Meta:
+        verbose_name_plural = 'hashtags'
