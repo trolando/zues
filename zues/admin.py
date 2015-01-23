7a79ba7451117ee98dcbdb2be8f2879f21747185
Tom van Dijk <tom@tvandijk.nl>
2015-01-23 23:37:13 +0100

    Added actions to admin

--- zues/admin.py
+++ zues/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django import forms
-from zues.models import PolitiekeMotie, ActuelePolitiekeMotie, Organimo, Resolutie, Amendement, HRWijziging, Login, Tijden, Categorie
+from zues.models import PolitiekeMotie, ActuelePolitiekeMotie, Organimo, Resolutie, Amendement, HRWijziging, Login, Tijden, Categorie, Stuk
 from solo.admin import SingletonModelAdmin
 
 
@@ -11,9 +11,35 @@ class MotieForm(forms.ModelForm):
         }
 
 
+def ingediend(modeladmin, request, queryset):
+    queryset.update(status=Stuk.INGEDIEND)
+ingediend.short_description = "Mark selected as Ingediend"
+
+
+def verwijderen(modeladmin, request, queryset):
+    queryset.update(status=Stuk.VERWIJDERD)
+verwijderen.short_description = "Mark selected as Verwijderd"
+
+
+def accepteren(modeladmin, request, queryset):
+    queryset.update(status=Stuk.GEACCEPTEERD)
+accepteren.short_description = "Mark selected as Geaccepteerd"
+
+
+def repareren(modeladmin, request, queryset):
+    queryset.update(status=Stuk.REPAREREN)
+repareren.short_description = "Mark selected as Repareren"
+
+
+def publiek(modeladmin, request, queryset):
+    queryset.update(status=Stuk.PUBLIEK)
+publiek.short_description = "Mark selected as Publiek"
+
+
 class MotieAdmin(admin.ModelAdmin):
     list_display = ('titel', 'status', 'admin_opmerkingen', 'categorie', 'boeknummer', 'eigenaar', 'woordvoerder', 'laatsteupdate')
     list_editable = ('categorie', 'admin_opmerkingen', 'boeknummer', 'status')
+    actions = [ingediend, verwijderen, accepteren, repareren, publiek]
 
     def get_changelist_form(self, request, **kwargs):
         kwargs.setdefault('form', MotieForm)
