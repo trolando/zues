f07b6b2e9cb316cf3c097a3698beadbf521dbec7
Wouter Kobes <w.j.kobes@student.utwente.nl>
2021-10-08 20:55:51 +0200

    Silence Recaptcha warning in travis

--- zuessite/travis.py
+++ zuessite/travis.py
@@ -2,4 +2,8 @@ from __future__ import absolute_import
 
 from zuessite.settings import *
 
+# Key only used for builds.
 SECRET_KEY = 'travistravistravistravistravistravistravistravistravis'
+
+# We do not need real Recaptcha keys in development.
+SILENCED_SYSTEM_CHECKS = ['captcha.recaptcha_test_key_error']
